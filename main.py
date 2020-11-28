65ddfa43e31b486971b4e191df22aafa3b335bf1
Tareq Judeh <tareq.judeh@outlook.com>
2020-11-28 20:01:41 +0200

    Added key events, which control Snake movements.

--- main.py
+++ main.py
@@ -9,15 +9,25 @@ screen.bgcolor("black")
 # Turn off tracer (animation while turtle is moving):
 screen.tracer(0)
 
+# Instantiating obj snake from class Snake():
 snake = Snake()
+
+# Key listening events:
+screen.listen()
+screen.onkey(snake.direction_up, "Up")
+screen.onkey(snake.direction_down, "Down")
+screen.onkey(snake.direction_right, "Right")
+screen.onkey(snake.direction_left, "Left")
+
 
 game_on = True
 while game_on:
     # refresh the screen each time, and after 0.2 delay:
     screen.update()
-    sleep(0.2)
+    sleep(0.1)
 
     snake.move()
+
 
 
 screen.exitonclick()
---
+++ snake.py
@@ -0,0 +1,46 @@
+from turtle import Turtle
+
+START_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+UP = 90
+DOWN = 270
+RIGHT = 0
+LEFT = 180
+
+class Snake:
+    # Creating a starting position for the snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    # Creating a snake body:
+    def create_snake(self):
+        for position in START_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.segments.append(new_segment)
+
+    def move(self):
+        for seg_num in range(len(self.segments) - 1, 0, - 1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(20)
+
+    def direction_up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def direction_down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def direction_right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def direction_left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
